dc56801474028b4098ea48490f3fb0d346275662
kirill-zavelev <kiri4by@gmail.com>
2020-06-09 20:39:57 +0300

    fix for getResponseBody test

--- src/test/java/model/User.java
+++ src/test/java/model/User.java
@@ -1,10 +1,12 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class User {
-
     private Integer id;
     private String name;
     private String username;
